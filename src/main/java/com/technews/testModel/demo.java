651b19936feb6f54bc3a9423fd7e66f57e11f855
Alexis Zaragoza Jr <alexis.oakland21@gmail.com>
2023-01-04 22:55:54 -0800

    user class created

---
+++ src/main/java/com/technews/testModel/demo.java
@@ -0,0 +1,48 @@
+package com.technews.testModel;
+
+import java.util.Objects;
+
+public class demo {
+    private String name;
+    private int age;
+
+    public demo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        demo demo = (demo) o;
+        return age == demo.age && Objects.equals(name, demo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
